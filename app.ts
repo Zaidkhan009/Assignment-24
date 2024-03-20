81996222d33e3d0a3b3a185f234421612b60ba65
Zaid khan <160598429+Zaidkhan009@users.noreply.github.com>
2024-03-20 06:50:07 +0500

    Add files via upload

---
+++ app.ts
@@ -0,0 +1,60 @@
+// let car: string = 'subaru';
+// let age: number = 25;
+// let numbers: number[] = [1,2,3,4,];
+
+
+// //  1:Equality  (True)
+// console.log("Is car == 'subaru'? i pridict True.");
+// console.log(car == 'subaru'); 
+
+// // 2:Strict Equality  (False)
+// console.log("Is car === 'subaru'? i pridict False.");
+// console.log(car === 'subaru'); 
+
+// //  3:Inequality (True)
+// console.log("Is car != 'Toyota'? i pridict True.");
+// console.log(car != 'Toyota'); 
+
+// // 4:Strict inequality (False)
+// console.log("Is car !== 'subaru'? i pridict False.");
+// console.log(car !== 'subaru');
+
+// // 5: Lowercase comparison (True)
+// console.log("Is car.toLowerCase() == 'subaru'? i pridict True.");
+// console.log(car.toLowerCase() == 'subaru'); 
+
+// // 6: Lowercase comparison (False)
+// console.log("Is car === car.toLowerCase()? i pridict False.");
+// console.log(car === car.toLowerCase());
+
+// // 7: Equality (True)
+// console.log("Is age == 25? i pridict True.");
+// console.log(age == 25);
+
+// // 8: Inequality (False)
+// console.log("Is age != 30? i pridict True.");
+// console.log(age != 30);
+
+// // 9:Greater than (False)
+// console.log("Is age > 30? i pridict False.");
+// console.log(age > 30);
+
+// // 10:Less than or equal (True)
+// console.log("Is age <= 25? i pridict True.");
+// console.log(age <= 25);
+
+// // 11: And (True)
+// console.log("Is age > 20 && age < 30? i pridict True.");
+// console.log(age > 20 && age < 30);
+
+// // 12: OR (False)
+// console.log("Is age > 30 || age < 18? i pridict False.");
+// console.log(age > 30 || age < 18);
+
+//  // 13: In array (True)
+// console.log("Is 3 in numbers? i pridict True.");
+// console.log(3 in numbers);
+
+// // 14: Not in array (False)
+// console.log("Is 5 not in numbers? i pridict True.");
+// console.log(5 not in numbers);
